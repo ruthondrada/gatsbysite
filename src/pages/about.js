a30f353592a601e4b0491214b6e62451d86dd24f
Ruth O <ondrada.ruth@gmail.com>
2022-09-04 04:17:44 +0800

    added links in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,8 @@ const About = () =>
     return (
         <>
         <h1> This is my about!</h1>
+        <p>My name is Ruth! </p>
+        <a href="/">Home</a> <a href="/about">About</a>
         </>
     )
 };
