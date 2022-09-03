6e259d907bddbf9cdc2b53694aa5c28209c218d0
Ruth O <ondrada.ruth@gmail.com>
2022-09-04 04:13:05 +0800

    my first commit for my website

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+//create component
+const About = () =>
+{
+    return (
+        <>
+        <h1> This is my about!</h1>
+        </>
+    )
+};
+
+//export
+
+export default About;
