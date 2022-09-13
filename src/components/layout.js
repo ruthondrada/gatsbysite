d80e1fd37b268fb86b9edc0221b59fd198a3a401
Ruth O <ondrada.ruth@gmail.com>
2022-09-13 23:38:52 +0800

    added blog page and a graphql query in page components

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,6 +31,8 @@ const Layout = ({pageTitle, children}) => {
                         <Link to="/" className={navLinkText}>Home</Link></li>
                     <li className={navLinkItem}>
                         <Link to="/about" className={navLinkText}>About</Link></li>
+                    <li classNmae={navLinkItem}>
+                        <Link to="/blog" className={navLinkText}>Blog</Link></li>
                 </ul>
             </nav>
             <main>
